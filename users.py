5282700a9830627a27cd6d8cb8700dae9924b809
Jay Shihora <76250028+Jayshihora@users.noreply.github.com>
2026-04-01 12:28:52 +0530

    User data Manually Added

---
+++ users.py
@@ -0,0 +1,52 @@
+# users.py — Fake user profiles for demo
+
+users = {
+    "ramesh": {
+        "name": "Ramesh (Retired, 65)",
+        "past_amounts": [200, 500, 150, 300, 450, 600, 250],
+        "known_payees": ["Kirana Store", "Son Amit", "Electricity Board"],
+        "usual_hours": [9, 10, 11, 14, 15, 16],
+        "usual_location": "Pune",
+        "language": "hindi"
+    },
+    "priya": {
+        "name": "Priya (Small Merchant, 32)",
+        "past_amounts": [1000, 2000, 1500, 800, 1200],
+        "known_payees": ["Wholesaler Raju", "Rent Landlord", "Staff Salary"],
+        "usual_hours": [8, 9, 10, 17, 18, 19],
+        "usual_location": "Mumbai",
+        "language": "english"
+    },
+    "arjun": {
+        "name": "Arjun (Student, 21)",
+        "past_amounts": [50, 100, 200, 150, 800, 700, 900, 1100],
+        "known_payees": ["Zomato", "Friend Rohit", "College Canteen","Swiggy","zepto","Blinkit"],
+        "usual_hours": [12, 13, 19, 20, 21],
+        "usual_location": "Pune",
+        "language": "english"
+    },
+     "jay": {
+        "name": "Jay (Student, 22)",
+        "past_amounts": [50, 150, 250, 500, 800, 600, 1000],
+        "known_payees": ["Zomato", "Friend Rohit", "College Canteen","Swiggy","zepto","Blinkit"],
+        "usual_hours": [12, 13, 19, 20, 21],
+        "usual_location": "Pune",
+        "language": "english"
+    },
+     "pooja": {
+        "name": "Pooja (Student, 25)",
+        "past_amounts": [500, 1100, 2000, 1500, 800,],
+        "known_payees": ["Zomato", "Friend Rohit", "College Canteen","Swiggy","zepto","Blinkit"],
+        "usual_hours": [12, 13, 19, 20, 21],
+        "usual_location": "Pune",
+        "language": "english"
+    },
+    "kirtan": {
+        "name": "Kirtan (Student, 25)",
+        "past_amounts": [500, 1100, 2100, 1500, 600, 550, 450, 700],
+        "known_payees": ["Zomato", "Friend Rohit", "College Canteen","Swiggy","zepto","Blinkit"],
+        "usual_hours": [12, 13, 19, 20, 21],
+        "usual_location": "Pune",
+        "language": "english"
+    }
+}
